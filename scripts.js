fc8f7e615405466655d9576e6937c2ec7623bc36
kpisti18 <k.istvanzoltan@gmail.com>
2020-04-23 16:43:31 +0200

    insert first part

---
+++ scripts.js
@@ -0,0 +1,12 @@
+let name = "Pisti";
+
+console.log(name + "'s apples");
+console.log("Hello World!");
+
+//This is a single line JavaScript comment
+
+/*
+This is a multiline
+JavaScript comment
+*/ 
+
